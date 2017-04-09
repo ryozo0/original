eec9ba3301322fff417d04b4a64b5add728e14a8
Ryosuke Okada <tobiuokun@gmail.com>
2017-04-09 06:28:16 +0000

    first commit

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,58 @@
+class TweetsController < ApplicationController
+  before_action :set_tweet, only: [:edit, :update, :destroy]
+  
+  def index
+    @tweets = Tweet.all
+  end
+
+  def new
+    if params[:back]
+      @tweet = Tweet.new(tweets_params)
+    else
+      @tweet = Tweet.new
+    end
+  end
+
+  def create
+    @tweet = Tweet.create(tweets_params)
+    if @tweet.save
+      redirect_to tweets_path, notice: "投稿しました！"
+    else
+      render 'new'
+    end
+  end
+  
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+  
+  def update
+    @tweet = Tweet.find(params[:id])
+    if @tweet.update(tweets_params)
+      redirect_to tweets_path, notice: "編集しました！"
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @tweet = Tweet.find(params[:id])
+    @tweet.destroy
+    redirect_to tweets_path, notice: "削減しました！"
+  end
+  
+  def confirm
+    @tweet = Tweet.new(tweets_params)
+    render :new if @tweet.invalid?
+  end
+  
+
+  private
+    def tweets_params
+      params.require(:tweet).permit(:content)
+    end
+  
+    def set_tweet
+      @tweet = Tweet.find(params[:id])
+    end
+end
